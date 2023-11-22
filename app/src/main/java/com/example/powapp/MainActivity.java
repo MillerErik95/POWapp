b9d96d01962dee0ec5aef1052ef16221c54c06c1
Alexander Emanuel Flores Martinez <alexander.flores-martinez@ucdenver.edu>
2023-11-22 11:47:16 -0700

    New Functionality and Formatting
    The app now uses a philosophy quotes api from Karan Dahiya to fetch quotes and authors
    
    for now it chooses a quote at random every time
    
    made some formatting changes to remove redundancy

--- app/src/main/java/com/example/powapp/MainActivity.java
+++ app/src/main/java/com/example/powapp/MainActivity.java
@@ -1,67 +1,52 @@
 package com.example.powapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import java.util.Calendar;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.ImageView;
 import android.content.ClipData;
 import android.content.ClipboardManager;
-import android.widget.Toast;
 import android.content.Context;
-import android.widget.Toolbar;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView quoteTextView;
+    private ImageView backgroundImageView;
+    private ImageButton shareButton;
+
+    private String quote;
+    private String author;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calendar calendar = Calendar.getInstance();
-        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        quoteTextView = findViewById(R.id.quoteTextView);
+        backgroundImageView = findViewById(R.id.backgroundImageView);
+        shareButton = findViewById(R.id.shareButton);
 
-        ImageView backgroundImageView = findViewById(R.id.backgroundImageView);
+        new FetchQuoteTask().execute();
 
-        switch (dayOfWeek) {
-            case Calendar.SUNDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.sunday_background);
-                break;
-            case Calendar.MONDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.monday_background);
-                break;
-            case Calendar.TUESDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.tuesday_background);
-                break;
-            case Calendar.WEDNESDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.wednesday_background);
-                break;
-            case Calendar.THURSDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.thursday_background);
-                break;
-            case Calendar.FRIDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.friday_background);
-                break;
-            case Calendar.SATURDAY:
-                backgroundImageView.setBackgroundResource(R.drawable.saturday_background);
-                break;
-        }
-
-        TextView quoteTextView = findViewById(R.id.quoteTextView);
-        ImageButton shareButton = findViewById(R.id.shareButton);
-
-        String quote = "This is probably the deepest and most significant philosophical quote you have ever read";
-        String author = "Sigma giga chad";
-        String url = "https://youtu.be/dQw4w9WgXcQ?si=NBGWWnWDvQIIgSVU";
-        String formattedQuote = String.format("\"%s\"\n\n- %s", quote, author);
-
-        quoteTextView.setText(formattedQuote);
-
+        setDayOfWeekBackground();
         quoteTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,17 +54,84 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String copiedText = formattedQuote + "\n\nLearn More: \"" + url + "\"";
+                String copiedText = String.format("\"%s\"\n\n- %s\n\nLearn More: \"%s\"", quote, author, url);
                 ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData clipData = ClipData.newPlainText("POW App Quote", copiedText);
                 clipboardManager.setPrimaryClip(clipData);
-                //Toast.makeText(MainActivity.this, "Quote copied to clipboard", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void setDayOfWeekBackground() {
+        Calendar calendar = Calendar.getInstance();
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        int[] dayBackgrounds = {
+                R.drawable.sunday_background,
+                R.drawable.monday_background,
+                R.drawable.tuesday_background,
+                R.drawable.wednesday_background,
+                R.drawable.thursday_background,
+                R.drawable.friday_background,
+                R.drawable.saturday_background
+        };
+
+        int background = dayBackgrounds[dayOfWeek - 1];
+        backgroundImageView.setBackgroundResource(background);
+    }
+
+    public class Quote {
+        private String source;
+        private String quote;
+
+        public String getSource() {
+            return source;
+        }
+
+        public String getQuote() {
+            return quote;
+        }
+    }
+
+    interface QuoteService {
+        @GET("quotes")
+        Call<List<Quote>> getQuotes();
+    }
+
+    private class FetchQuoteTask extends AsyncTask<Void, Void, List<Quote>> {
+        @Override
+        protected List<Quote> doInBackground(Void... params) {
+            try {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://philosophy-quotes-api.glitch.me/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                QuoteService service = retrofit.create(QuoteService.class);
+                Call<List<Quote>> call = service.getQuotes();
+                return call.execute().body();
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error fetching quotes", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Quote> quotes) {
+            if (quotes != null && !quotes.isEmpty()) {
+                int randomIndex = new Random().nextInt(quotes.size());
+                Quote randomQuote = quotes.get(randomIndex);
+
+                author = randomQuote.getSource();
+                quote = randomQuote.getQuote();
+
+                url = "https://en.wikipedia.org/wiki/" + author;
+                String formattedQuote = String.format("\"%s\"\n\n- %s", quote, author);
+                quoteTextView.setText(formattedQuote);
+            }
+        }
     }
 }
