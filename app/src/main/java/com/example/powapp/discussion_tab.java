10081c3e789043c7fc9476ab56d7be714e551317
your_username <your_email@example.com>
2023-12-06 13:35:55 -0700

    button now takes you to discussion tab

---
+++ app/src/main/java/com/example/powapp/discussion_tab.java
@@ -0,0 +1,14 @@
+package com.example.powapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class discussion_tab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discussion_tab);
+    }
+}
