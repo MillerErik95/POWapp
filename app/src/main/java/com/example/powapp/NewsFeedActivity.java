240e156e48a79412e6052de21d3324041e135f9f
MillerErik95 <MillerErik95@gmail.com>
2023-12-06 19:52:38 -0700

    new new

--- app/src/main/java/com/example/powapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/powapp/NewsFeedActivity.java
@@ -16,7 +16,7 @@ public class NewsFeedActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private NewsFeedAdapter adapter;
-    private List<String> newsItems;
+    private List<NewsItem> newsItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -26,7 +26,14 @@ public class NewsFeedActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.news_feed_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        newsItems = Arrays.asList("News Item 1", "News Item 2", "News Item 3");
+        newsItems = Arrays.asList(
+                new NewsItem("Friedrich Nietzsche: The Philosopher Who Took On The World", "https://www.youtube.com/embed/1KkuwWn7Dmo?si=A9DFE1cTQDIdzMJe", "https://i9.ytimg.com/vi/1KkuwWn7Dmo/maxresdefault.jpg?v=64cfd9fd&sqp=CIyrxKsG&rs=AOn4CLARCuGMX6fgDrMk4x3K2XPIsypmxw"),
+                new NewsItem("Kierkegaard's Philosophy - How To Believe In Yourself Against All Odds", "https://www.youtube.com/embed/fr8i1T1vy_0?si=C6fZjuEk_yOEP24v", "https://i9.ytimg.com/vi/fr8i1T1vy_0/maxresdefault.jpg?v=649d840b&sqp=CLitxKsG&rs=AOn4CLBG2GtGkHhuHagwLbnRBV7m8ISs0w"),
+                new NewsItem("What Can You Really Know? The Philosophy of Michel Foucault", "https://www.youtube.com/embed/xxvydQnocTw?si=9FBeG9NyCh8rPW5P", "https://i9.ytimg.com/vi/xxvydQnocTw/maxresdefault.jpg?v=6448088d&sqp=CLitxKsG&rs=AOn4CLD361xgGr-itXIVmx651Se7WtIS7g"),
+                new NewsItem("The Greatest Philosopher You've Never Heard Of - Reiner Schürmann", "https://www.youtube.com/embed/8IVRkbC5Ux8?si=3-5tCI0cd0fZbno0", "https://i9.ytimg.com/vi/8IVRkbC5Ux8/maxresdefault.jpg?v=63e6bdf2&sqp=CLitxKsG&rs=AOn4CLC1alrEwzoO7rhwChY6NneR-aQnhQ"),
+                new NewsItem("Absurdism - Is Life Meaningless? (The Philosophy of Albert Camus)", "https://www.youtube.com/embed/68bex8T-ZWE?si=4od7H5LECHtLp9tw", "https://i9.ytimg.com/vi/68bex8T-ZWE/maxresdefault.jpg?v=64046c63&sqp=COSvxKsG&rs=AOn4CLC-4tH-zJNV8X7MCdn9o5ySWaZvxQ"),
+                new NewsItem("Why The World is Falling Apart - Ep 1 - The Internet & Pandora's Box", "https://www.youtube.com/embed/j_2Lb7CUbR0?si=VElfXuYTV2oaJXh6", "https://i9.ytimg.com/vi/j_2Lb7CUbR0/maxresdefault.jpg?v=654f2cb2&sqp=COSvxKsG&rs=AOn4CLBeYSWiy89nS35EBJrJuFi5qaJ8Yg")
+        );
 
         adapter = new NewsFeedAdapter(newsItems);
         recyclerView.setAdapter(adapter);
--- app/src/main/java/com/example/powapp/NewsFeedAdapter.java
+++ app/src/main/java/com/example/powapp/NewsFeedAdapter.java
@@ -3,16 +3,22 @@ package com.example.powapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.NewsViewHolder> {
 
-    private List<String> newsItems;   // Data Source
+    private List<NewsItem> newsItems;
 
-    public NewsFeedAdapter(List<String> newsItems) {
+    public NewsFeedAdapter(List<NewsItem> newsItems) {
         this.newsItems = newsItems;
     }
 
@@ -25,8 +31,39 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.NewsVi
 
     @Override
     public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
-        String item = newsItems.get(position);
-        holder.tvNewsItem.setText(item);
+        NewsItem item = newsItems.get(position);
+        holder.tvNewsItem.setText(item.getTitle());
+
+        // Load the thumbnail image using Picasso
+        Picasso.get().load(item.getThumbnailUrl()).into(holder.imageNewsThumbnail);
+
+        // Configure the WebView to load the video URL
+        holder.webView.getSettings().setJavaScriptEnabled(true); // Enable JavaScript for the WebView
+
+        // Set a WebViewClient to override default behavior
+        holder.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // Hide the loading thumbnail once the page is fully loaded
+                holder.imageNewsThumbnail.setVisibility(View.GONE);
+            }
+        });
+
+        // Construct the HTML content for the WebView with the iframe embed code
+        String embedUrl = item.getVideoUrl(); // Make sure this is the embed URL
+        String frameVideo = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"" + embedUrl +
+                "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+
+        // Load the HTML content into the WebView
+        holder.webView.loadData(frameVideo, "text/html", "UTF-8");
+
+        // Optional: Set an OnClickListener on the thumbnail to reload the video when clicked
+        holder.imageNewsThumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.webView.loadData(frameVideo, "text/html", "UTF-8");
+            }
+        });
     }
 
     @Override
@@ -36,10 +73,14 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.NewsVi
 
     public static class NewsViewHolder extends RecyclerView.ViewHolder {
         TextView tvNewsItem;
+        ImageView imageNewsThumbnail;
+        WebView webView;
 
         public NewsViewHolder(View itemView) {
             super(itemView);
             tvNewsItem = itemView.findViewById(R.id.tvNewsItem);
+            imageNewsThumbnail = itemView.findViewById(R.id.imageNewsThumbnail);
+            webView = itemView.findViewById(R.id.webView);
         }
     }
 }
